587643b5186c7750773ab2ebf51651d0aa8a75a3
Shannon Tsaltas <shannontsaltas@gmail.com>
2014-08-23 15:01:13 -0500

    completed input form with hardcoded story and words

--- mad_libs/views.py
+++ mad_libs/views.py
@@ -1,7 +1,25 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from mad_libs import app
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+
+@app.route("/", methods=["GET"])
+def input_form():
+	input_list = [('1','noun'), ('4','adjective')]
+	return render_template("input_form.html", inputs=input_list)
+
+@app.route("/", methods=["POST"])
+def display_story():
+	filename = "mad_libs/static/story.txt"
+
+	with open(filename, "r") as f:
+		story_text = f.read().strip()
+	
+	story_text = story_text.strip()
+	story_text = story_text.split()
+	story_text[1] = request.form["1"]
+	story_text[4] = request.form["4"]
+	story_text = ' '.join(story_text)
+	story_text = story_text + '.'
+	
+	return render_template("display_story.html", story=story_text)
