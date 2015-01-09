8ae1dfde39fbe9e1a1b455f7c7d447217c801295
Shannon Tsaltas <shannontsaltas@gmail.com>
2015-01-09 10:58:48 -0600

    moved into deployment configuratoin

--- mad_libs/__init__.py
+++ mad_libs/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-config_path = os.environ.get("CONFIG_PATH", "mad_libs.config.DevelopmentConfig")
+config_path = os.environ.get("CONFIG_PATH", "mad_libs.config.DeploymentConfig")
 app.config.from_object(config_path)
 
 import views
